6ec187b15365085e6f258c789aa91189c2f105b0
Zivitesh-555 <kashmiryan463@gmail.com>
2026-05-10 16:31:30 +0530

    chnged entire code of app.py nd added fil tasks.json

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request, redirect
+import json
+import os
+
 app = Flask(__name__)
-tasks = []
+
+FILE = "tasks.json"
+
+# Load tasks
+if os.path.exists(FILE):
+    with open(FILE, "r") as f:
+        tasks = json.load(f)
+else:
+    tasks = []
+
+def save_tasks():
+    with open(FILE, "w") as f:
+        json.dump(tasks, f)
 
 @app.route("/")
 def home():
@@ -9,14 +24,20 @@ def home():
 @app.route("/add", methods=["POST"])
 def add():
     task = request.form.get("task")
+
     if task:
         tasks.append(task)
+        save_tasks()
+
     return redirect("/")
 
 @app.route("/delete/<int:index>")
 def delete(index):
-    if 0<=index<len(tasks):
+    if 0 <= index < len(tasks):
         tasks.pop(index)
+        save_tasks()
+
     return redirect("/")
+
 if __name__ == "__main__":
     app.run(debug=True)
